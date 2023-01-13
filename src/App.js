426790bfafebd268bcb7a41d653a544a92e914dc
John Panton <johnpanton29@outlook.com>
2023-01-13 11:06:08 +0000

    finished react intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    // 1️⃣
+import './App.css';           // 2️⃣
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {              // 3️⃣
+  // we can write JS here :)  // 4️⃣
+  let name="John"
+  let age=20
+  let numArray=[,10,15,20,25]
+
+  //Testing out react here
+  const calculateTotal = function (numArray) {
+    const total=numArray.reduce((runningTotal,num)=>{
+      return runningTotal+num
+    },0)
+    return total
+  };
+  const answer=calculateTotal(numArray)
+
+
+  return (                    // 5️⃣
+    //  <h1>Hello World!</h1>     // 6️⃣
+    <div>
+  		<h1 className="title">Hello World!</h1>
+  		<p> My name is {name} and I'm {age} years old</p>
+      <p>The sum of the array is: {answer}</p>
+  	</div>
+      
+  );                          // 7️⃣
+}                             // 8️⃣
 
 export default App;
